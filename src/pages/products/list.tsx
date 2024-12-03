b640ad4e49244582ded902e45799ca6a2c307762
ihsanrm99 <ihsanrahmatm@gmail.com>
2024-12-03 23:56:49 +0700

    feat[listing records]: Using the useList Hook

---
+++ src/pages/products/list.tsx
@@ -0,0 +1,30 @@
+import { useList } from "@refinedev/core";
+
+export const ListProducts = () => {
+  const { data, isLoading } = useList({
+    resource: "products",
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {data?.data?.map((product) => (
+          <li key={product.id}>
+            <p>
+              {product.name}
+              <br />
+              Price: {product.price}
+              <br />
+              Material: {product.material}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
