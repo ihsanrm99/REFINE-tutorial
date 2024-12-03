d994202675c21ea14dc20473732341721b9e9ffe
ihsanrm99 <ihsanrahmatm@gmail.com>
2024-12-03 23:53:41 +0700

    feat[listing records]: Implementing the getList Method

--- src/providers/data-provider.ts
+++ src/providers/data-provider.ts
@@ -5,17 +5,22 @@ import type {
   DataProvider,
   DeleteOneParams,
   DeleteOneResponse,
-  GetListParams,
-  GetListResponse,
 } from "@refinedev/core";
 
 const API_URL = "https://api.fake-rest.refine.dev";
 
 export const dataProvider: DataProvider = {
-  getList: function <TData extends BaseRecord = BaseRecord>(
-    params: GetListParams,
-  ): Promise<GetListResponse<TData>> {
-    throw new Error("Function not implemented.");
+  getList: async ({ resource, pagination, filters, sorters, meta }) => {
+    const response = await fetch(`${API_URL}/${resource}`);
+
+    if (response.status < 200 || response.status > 299) throw response;
+
+    const data = await response.json();
+
+    return {
+      data,
+      total: 0,
+    };
   },
   getOne: async ({ resource, id, meta }: any) => {
     const response = await fetch(`${API_URL}/${resource}/${id}`);
